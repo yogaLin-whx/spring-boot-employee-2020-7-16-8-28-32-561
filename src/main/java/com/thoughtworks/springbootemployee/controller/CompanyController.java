19c07e1a83f7dc30aa3e81bfee7d0e5c1faaedb9
wyuefzhuol <1527169088@qq.com>
2020-07-28 22:10:08 +0800

    [Restful practice][Eric] feature: create a class CompanyController and CompanyService

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+}
