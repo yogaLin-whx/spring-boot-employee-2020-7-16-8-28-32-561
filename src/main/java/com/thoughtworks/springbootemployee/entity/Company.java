2c884acf62e26fa1247795efb0ef849edb27d53d
wyuefzhuol <1527169088@qq.com>
2020-07-28 22:06:05 +0800

    [Restful practice][Chris] feature: create a class Company in Entity

---
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Company.java
@@ -0,0 +1,42 @@
+package com.thoughtworks.springbootemployee.entity;
+
+import java.util.List;
+
+public class Company {
+    private int id;
+    private String companyName;
+    private int employeesNumber;
+    private List<Employee> employees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
