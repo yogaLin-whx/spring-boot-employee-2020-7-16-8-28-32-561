ce4b212e232a993a4a426b456b970318cc042ea8
wyuefzhuol <1527169088@qq.com>
2020-07-28 22:11:44 +0800

    [Restful practice][Chris] feature: create a method CompanyController.getCompanies

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,9 +1,23 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping("/")
+    public List<Company> getCompanies(){
+        return  companyService.getCompanies();
+    }
 }
--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CompanyService {
+
+    @Autowired
+    CompanyRepository companyRepository;
+
+    public List<Company> getCompanies() {
+        return  companyRepository.getCompanies();
+    }
 }
