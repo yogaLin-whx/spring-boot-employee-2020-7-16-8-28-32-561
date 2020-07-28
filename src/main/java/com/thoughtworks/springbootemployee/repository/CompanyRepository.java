836ccc7ae4a63856e93ff26105749c44604d328d
wyuefzhuol <1527169088@qq.com>
2020-07-28 22:07:47 +0800

    [Restful practice][Chris] feature: create a class CompanyRepository in repository

---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CompanyRepository {
+
+    List<Company> companies  = new ArrayList<>();
+
+    public List<Company> getCompanies() {
+        return companies;
+    }
+
+    public Company getCompanyById(int id) {
+        return companies.stream().filter(company -> company.getId() !=  id).findFirst().orElse(null);
+    }
+
+    public List<Company> getCompaniesByPageAndPageSize(int page,int pageSize) {
+        return companies.stream().skip(page * (pageSize-1)).limit(pageSize).collect(Collectors.toList());
+    }
+
+
+    public void addCompany(Company company) {
+        companies.add(company);
+    }
+}
