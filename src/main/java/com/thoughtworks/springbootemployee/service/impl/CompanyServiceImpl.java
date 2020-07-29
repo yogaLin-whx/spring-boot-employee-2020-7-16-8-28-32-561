442a2e992f602bf4819814539ddf5e524f189a62
WUCH8 <944204910@qq.com>
2020-07-29 18:47:29 +0800

    [Spring practice][Chris] refactor: modify getEmployeeOfCompany .get to .orElse

--- src/main/java/com/thoughtworks/springbootemployee/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/impl/CompanyServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyServiceImpl implements CompanyService {
@@ -30,7 +31,8 @@ public class CompanyServiceImpl implements CompanyService {
         return companyRepository.findAllById(Collections.singleton(id))
                 .stream()
                 .map(Company::getEmployees)
-                .findFirst().get();
+                .findAny()
+                .orElse(null);
     }
 
     public void addCompany(Company company) {
