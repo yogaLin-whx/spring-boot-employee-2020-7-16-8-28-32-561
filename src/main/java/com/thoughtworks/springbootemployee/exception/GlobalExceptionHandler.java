5256a540add1037f1b1a5014797b471aa18a45c6
WUCH8 <944204910@qq.com>
2020-07-31 10:07:11 +0800

    [Spring Boot][Chris&Oliver]feature:handleValidationFailure

--- src/main/java/com/thoughtworks/springbootemployee/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.springbootemployee.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +17,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CompanyNotFoundException.class)
     void companyNotFoundException() {
 
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public @ResponseBody
+    List<String> handleValidationFailure(MethodArgumentNotValidException excetion){
+        return excetion
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ":" + fieldError.getDefaultMessage())
+                .collect(Collectors.toList());
     }
 
 }
