8eabce3ce805052e4a66debedbcd79af908fdd37
yogaLin-whx <whx13058048980>
2020-08-01 19:13:14 +0800

    [Restful practice][Chris]refactor:simplify  code

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -2,12 +2,7 @@ package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
